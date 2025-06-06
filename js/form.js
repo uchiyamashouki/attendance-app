cede477d4374ea1ed7383034a4c41f831263ae5c
uchiyamashouki <135842430+uchiyamashouki@users.noreply.github.com>
2025-06-07 05:37:06 +0900

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,10 +1,10 @@
 export function sendToGoogleForm(data) {
   const formData = new FormData();
-  formData.append("entry.1235081500", data.userName);
-  formData.append("entry.2025397394", data.pitches);
-  formData.append("entry.360519448", data.latitude);
-  formData.append("entry.792823488", data.longitude);
-  formData.append("entry.658296585", data.attendanceType);
+ formData.append("entry.1235081500", data.name);           // 氏名（正）
+  formData.append("entry.2085125757", data.attendance);     // 練習区分（正）
+  formData.append("entry.2025397394", data.pitches);        // 球数
+  formData.append("entry.360519448", data.latitude);        // 緯度
+  formData.append("entry.658296585", data.longitude);       // 経度（ID修正済み）
 
   fetch("https://docs.google.com/forms/d/e/1FAIpQLSegSTTLeL75exvNWpF2iEQqdP8nUC4p55TBLpjvPR1WGefBCA/formResponse", {
     method: "POST",
